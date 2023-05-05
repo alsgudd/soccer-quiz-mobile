2a38b0bedccf3f5b4da57f42fa0cc683c1c27421
peter <peterlee11@naver.com>
2023-05-05 15:37:46 +0900

    feat: Implement atoms during atomic design

---
+++ client/src/Atoms/index.ts
@@ -0,0 +1,25 @@
+import Button from './Button';
+import Checkbox from './Checkbox';
+import Div from './Div';
+import Input from './Input';
+import Label from './Label';
+import Li from './Li';
+import Select from './Select';
+import Span from './Span';
+import SubTitle from './SubTitle';
+import Title from './Title';
+import Ul from './Ul';
+
+export default {
+  Button,
+  Checkbox,
+  Div,
+  Input,
+  Label,
+  Li,
+  Select,
+  Span,
+  SubTitle,
+  Title,
+  Ul,
+};
---
+++ client/src/constant/index.ts
@@ -0,0 +1,26 @@
+export const PLATFORM_IOS = 'ios';
+export const PLATFORM_ANDROID = 'android';
+
+export const RESULT_PAGENAME = 'result';
+export const QUIZ_PAGENAME = 'quiz';
+export const LANDING_PAGENAME = 'landing';
+
+export const MULTIPLE_TYPE = 'multiple';
+export const DEFAULT_NUMBERS = 2; 
+export const ANY_DIFFICULTY = 'Any Difficulty';
+export const EASY_DIFFICULTY = 'easy';
+export const MEDIUM_DIFFICULTY = 'medium';
+export const HARD_DIFFICULTY = 'hard';
+
+export const CORRECT_COLOR = 'var(--green-400)'
+export const INCORRECT_COLOR = 'var(--red-400)'
+
+export const difficulties = [
+  ANY_DIFFICULTY,
+  EASY_DIFFICULTY,
+  MEDIUM_DIFFICULTY,
+  HARD_DIFFICULTY,
+];
+
+export const DIFFICULTY_SELECT_TEST_ID = 'difficulty-select'
+export const EXAMPLE_LABEL_TEST_ID = 'example-label'
