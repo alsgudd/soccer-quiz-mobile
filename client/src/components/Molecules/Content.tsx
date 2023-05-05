599fdd33f286378e9af5df3259d603e611a3ac09
peter <peterlee11@naver.com>
2023-05-05 16:26:02 +0900

    feat: Implement molecules during atomic design

---
+++ client/src/components/Molecules/Content.tsx
@@ -0,0 +1,57 @@
+import { ReactNode } from 'react';
+
+import { getQueryData } from 'src/utils';
+import Atoms from 'components/Atoms';
+
+export type ContentProps = {
+  height?: string;
+  marginTop?: string;
+  header?: ReactNode;
+  headerBorderBottom?: string;
+  headerRight?: ReactNode;
+  body?: ReactNode;
+  children?: ReactNode;
+};
+
+const Content = ({
+  height,
+  marginTop,
+  header,
+  headerBorderBottom,
+  headerRight,
+  children,
+}: ContentProps) => {
+  const { platform } = getQueryData();
+
+  return (
+    <Atoms.Div
+      height={height}
+      width="100%"
+      background="var(--white)"
+      marginTop={marginTop || '4px'}
+    >
+      {header && (
+        <Atoms.Div
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          padding={`var(--padding-${platform})`}
+          height="48px"
+          boxSizing="border-box"
+          borderBottom={headerBorderBottom || '1px solid var(--grey-100)'}
+        >
+          <Atoms.SubTitle>{header}</Atoms.SubTitle>
+          <Atoms.Div display="flex" alignItems="center" fontSize="20px">
+            {headerRight}
+          </Atoms.Div>
+        </Atoms.Div>
+      )}
+      <Atoms.Div padding={`var(--padding-${platform})`} height="100%">
+        {children}
+      </Atoms.Div>
+    </Atoms.Div>
+  );
+};
+
+
+export default Content;
