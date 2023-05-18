310a8a7689b9e2c05ac5e2272426906ac9da8272
peter <peterlee11@naver.com>
2023-05-18 15:54:51 +0900

    feat: mypage에서 사용할 record 받아오는 customHook 작성

---
+++ client/src/hooks/useMyPage.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+interface TRecord {
+  _id: string;
+  score: number;
+  quizTeam: string;
+}
+
+const useMyPage = () => {
+  const [record, setRecord] = useState<TRecord[]>([]);
+  const [status, setStatus] = useState(0);
+
+  useEffect(() => {
+    axios({
+      url: `${process.env.REACT_APP_SERVER_URL}/mypage/record`,
+      method: "GET",
+      withCredentials: true,
+    }).then((response) => { 
+      setRecord(response.data.record);
+      setStatus(200);
+    }).catch((e) => {
+      setStatus(404);
+    })
+  }, [])
+
+  return {
+    record,
+    status
+  }
+}
+
+export default useMyPage;
